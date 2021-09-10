55a4daeba81f5067b8e57b04fb2ed5260269396f
Zach Hoeken <hoeken@gmail.com>
2021-09-10 11:28:45 -0500

    small change to loadcell script and updated todo

--- test-rig/HX711-Reader/HX711-Reader.ino
+++ test-rig/HX711-Reader/HX711-Reader.ino
@@ -35,14 +35,12 @@ HX711 scale;
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("HX711 Scale v1.0");
+  //Serial.println("HX711 Scale v1.0");
+  //Serial.println("Kilograms,Calibration Factor"); //You can change this to kg but you'll need to refactor the calibration_factor
 
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
   scale.set_scale(calibration_factor); //This value is obtained by using the SparkFun_HX711_Calibration sketch
   scale.tare();	//Assuming there is no weight on the scale at start up, reset the scale to 0
-
-  Serial.println("Kilograms,Calibration Factor"); //You can change this to kg but you'll need to refactor the calibration_factor
-
 }
 
 void loop() {
@@ -61,4 +59,6 @@ void loop() {
     else if (temp == 't')
       scale.tare();  //Assuming there is no weight on the scale at start up, reset the scale to 0
   }
+
+  delay(1);
 }
